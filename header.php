e9e6289929d7d1c6e40357b60f8ab4ca6056889e
jngierke <jngierke@gmail.com>
2014-12-24 02:31:56 -0500

    Create header.php

---
+++ header.php
@@ -0,0 +1,38 @@
+<?php
+include_once("base.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php echo $title; ?></title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <link href="css/site.css" rel="stylesheet">
+    
+  </head>
+  <body>
+  	<header class="clearfix">
+		<div class="btn-group pull-right">
+		  <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+			<?php echo $_SESSION['username']; ?> <span class="caret"></span> 
+		  </button>
+		  <ul class="dropdown-menu" role="menu">
+			<li><a href="logout.php">Logout</a></li>
+		  </ul>
+		</div>
+		<div class="logo pull-left">
+			<img src="images/logo.garces.jpg" alt="Garces Family Foundation"/>
+		</div>
+	</header>
